8a94b479d8712746587b42baa88b67c2ea51d05a
enPlace <n.place@protonmail.com>
2021-04-09 19:15:28 -0300

    updating delete function

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let books = document.querySelectorAll("#book-list li .name");
 
-const bookList = document.querySelector("#book-list");
 
-let deleteButtons = document.querySelectorAll(".delete-button");
 
-deleteButtons.forEach(button => {
-    button.addEventListener('click', e=>{
-        const li = e.target.parentElement.parentElement;
-        li.parentNode.removeChild(li)
-    });
-});
+const deleteButtons = document.querySelectorAll(".delete-button"); 
 
+const bookList = document.querySelector("#book-list ul")
+
+bookList.addEventListener('click', e=>{
+    if(e.target.className=='delete-button'){
+        let listElement = e.target.parentElement.parentElement;
+        listElement.parentNode.removeChild(listElement)
+    }else{
+        console.log(e);
+    }
+})
