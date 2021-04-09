04352c5a0a609e5fb63c6ee8993b23d0f173e875
enPlace <n.place@protonmail.com>
2021-04-09 18:30:51 -0300

    delete button scripts

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let books = document.querySelectorAll("#book-list li .name");
 
-const bookList = document.querySelector("#book-list")
+const bookList = document.querySelector("#book-list");
+
+let deleteButtons = document.querySelectorAll(".delete-button");
+
+deleteButtons.forEach(button => {
+    button.addEventListener('click', e=>{
+        const li = e.target.parentElement.parentElement;
+        li.parentNode.removeChild(li)
+    });
+});
+
