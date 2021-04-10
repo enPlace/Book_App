bf09bcad6b1c94cc3df859defdd40f35fa4606c5
enPlace <n.place@protonmail.com>
2021-04-10 19:04:12 -0300

    adding new books with button and class names

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ const bookList = document.querySelector("#book-list ul")
 
 bookList.addEventListener('click', e=>{
     bookRow = e.target.parentElement.parentElement
-    if(e.target.className = "delete-button"){
+    if(e.target.className == "delete-button"){
       
         bookRow.parentNode.removeChild(bookRow)
     }});
 
-
-
-const addForm = document.querySelector("#add-book")
+addForm = document.querySelector("#add-book")
 
 addForm.addEventListener('submit', e=>{
-    e.preventDefault(); 
-   const value = document.querySelector("#bookform").value; 
-   console.log(value);
-   if(value == ""){
-   }else{
-   const newLi = document.createElement("li");
-   const newBook = document.createElement("span")
-   const newButton = document.createElement("span")
-
-   newBook.textContent=value;
-
-   newLi.appendChild(newBook)
-   newLi.appendChild(newButton)
-   bookList.appendChild(newLi)
-   }
+    e.preventDefault();
+    inputValue = document.querySelector("#bookform").value;
+    if (inputValue == ""){}
+    else{
+        const newLi = document.createElement("li")
+        const newBook= document.createElement("span")
+        const newSpan = document.createElement("span")
+        const newDelete = document.createElement("button")
+        newBook.textContent = inputValue
+        newBook.className = "name"
+        newSpan.className = "delete"
+        newDelete.className = "delete-button"
+        newDelete.textContent = "Remove"
+       
+        newSpan.style.float = "right"
+        newSpan.appendChild(newDelete)
+        newLi.appendChild(newBook)
+        newLi.appendChild(newSpan)
+        bookList.appendChild(newLi)
+    }
 })
+
