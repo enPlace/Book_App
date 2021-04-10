194b5460f8e2fd32cdbd0bce6aa7eb053af3ecf2
enPlace <n.place@protonmail.com>
2021-04-10 16:35:45 -0300

    js for adding items to list

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let books = document.querySelectorAll("#book-list li .name");
-
-
-
-const deleteButtons = document.querySelectorAll(".delete-button"); 
 
 const bookList = document.querySelector("#book-list ul")
 
 bookList.addEventListener('click', e=>{
-    if(e.target.className=='delete-button'){
-        let listElement = e.target.parentElement.parentElement;
-        listElement.parentNode.removeChild(listElement)
-    }else{
-        console.log(e);
-    }
+    bookRow = e.target.parentElement.parentElement
+    if(e.target.className = "delete-button"){
+      
+        bookRow.parentNode.removeChild(bookRow)
+    }});
+
+
+
+const addForm = document.querySelector("#add-book")
+
+addForm.addEventListener('submit', e=>{
+    e.preventDefault(); 
+   const value = document.querySelector("#bookform").value; 
+   console.log(value);
+   if(value == ""){
+   }else{
+   const newLi = document.createElement("li");
+   const newBook = document.createElement("span")
+   const newButton = document.createElement("span")
+
+   newBook.textContent=value;
+
+   newLi.appendChild(newBook)
+   newLi.appendChild(newButton)
+   bookList.appendChild(newLi)
+   }
 })
